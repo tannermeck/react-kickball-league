b6d9ce9795319273f85770bd5db2d61682b805c5
TANNER MECK <steve.meck246@gmail.com>
2021-12-07 14:48:09 -0800

    players now rendering to team detail page

--- src/views/TeamDetail.jsx
+++ src/views/TeamDetail.jsx
@@ -1,7 +1,32 @@
-function TeamDetail({ team }){
-    console.log('TEAM', team)
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getTeamById } from '../services/teams';
+
+function TeamDetail(){
+    const { id } = useParams();
+    const [team, setTeam] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadTeams(){
+            const teamDetail = await getTeamById(id)
+            setTeam(teamDetail)
+            setLoading(false)
+        }
+        loadTeams()
+    }, [])
+    console.log(team)
+    if (loading) return <h1>Loading Team Details...</h1>
+
     return (
-        <h1>Hello from TeamDetail</h1>
+        <>
+            <h1>{team.name}</h1>
+            <h3>{team.city}, {team.state}</h3>
+            <h2>Players:</h2>
+            {team.players.map(player => (
+                <h3 key={player.id}>{player.name}</h3>
+            ))}
+        </>
     )
 }
 export default TeamDetail;
