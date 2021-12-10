de70f5e990f0b549e526f023c3d6c8c6bac668a2
TANNER MECK <steve.meck246@gmail.com>
2021-12-10 09:23:59 -0800

    msw added to player/team list and detail

--- src/views/players/PlayerList.test.jsx
+++ src/views/players/PlayerList.test.jsx
@@ -1,6 +1,31 @@
 import { screen, render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
 import PlayerList from './PlayerList';
+
+const fakePlayer1 = {
+    id: 1, 
+    created_at: '2021-12-10T17:05:14.55043+00:00', 
+    name: 'Amber', 
+    position: 'organizer', 
+    team_id: 17
+}
+
+const worker = setupServer(
+    rest.get('https://bmugikqtddktbuecxyqi.supabase.co/rest/v1/players', (req, res, ctx) => {
+        return res(
+            ctx.json([fakePlayer1])
+        )
+    })
+);
+
+beforeAll(() => {
+    worker.listen()
+});
+afterAll(() => {
+    worker.close()
+});
 
 it('should render out a playerlist component', async () => { 
     const { container } = render(
@@ -8,8 +33,8 @@ it('should render out a playerlist component', async () => {
                                 <PlayerList />
                             </MemoryRouter>)
     const label = await screen.findByText('Players:', {exact: false})
-    
+    const player = await screen.findByText('Amber')
+    expect(player).toBeInTheDocument();
     expect(label).toBeInTheDocument();
     expect(container).toMatchSnapshot();
-
-})
+});
--- src/views/teams/TeamList.test.jsx
+++ src/views/teams/TeamList.test.jsx
@@ -1,6 +1,31 @@
 import { screen, render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
 import TeamList from './TeamList';
+
+const fakeTeam1 = {
+    id: 1, 
+    created_at: '2021-12-08T19:18:29+00:00', 
+    name: 'My Team', 
+    city: 'Portland', 
+    state: 'Or',
+    players: []
+}
+const worker = setupServer(
+    rest.get('https://bmugikqtddktbuecxyqi.supabase.co/rest/v1/teams', (req, res, ctx) => {
+        return res(
+            ctx.json([fakeTeam1])
+        )
+    })
+);
+
+beforeAll(() => {
+    worker.listen()
+});
+afterAll(() => {
+    worker.close()
+});
 
 it('should render out a team list component', async () => { 
     const { container } = render(
@@ -9,6 +34,8 @@ it('should render out a team list component', async () => {
                             </MemoryRouter>)
     await screen.findByText(/Loading.../i)
     const label = await screen.findByText('Teams:', {exact: false})
+    const teamName = await screen.findByText('My Team')
+    expect(teamName).toBeInTheDocument();
     expect(label).toBeInTheDocument();
     expect(container).toMatchSnapshot();
 })
